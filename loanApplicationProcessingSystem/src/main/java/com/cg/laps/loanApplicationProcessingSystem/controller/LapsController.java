ec7c5b1b2beec26890b96a945cfb6452950ce1bd
edvdivya <54656457+edvdivya@users.noreply.github.com>
2019-11-07 11:56:00 +0530

    boot with functionality

--- loanApplicationProcessingSystem/src/main/java/com/cg/laps/loanApplicationProcessingSystem/controller/LapsController.java
+++ loanApplicationProcessingSystem/src/main/java/com/cg/laps/loanApplicationProcessingSystem/controller/LapsController.java
@@ -73,7 +73,7 @@ public class LapsController {
 
 		//tally usernames here
 		
-		if (member.getUsername() == "y") {
+		if (member.getUsername() == "yyy") {
 			return new ResponseEntity<String>("Member not added", HttpStatus.ALREADY_REPORTED);
 		} else {
 			return new ResponseEntity<User>(member, HttpStatus.OK);
